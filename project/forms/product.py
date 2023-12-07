88a049b7f952f979d92405affbbc9df17163d79b
Fabio Solidade <solidadefab@gmail.com>
2023-12-07 15:17:04 -0300

    create product

--- project/forms/product.py
+++ project/forms/product.py
@@ -1,12 +1,14 @@
 from django import forms
 from project.models import Product
 
+
 class ProductForm(forms.ModelForm):
+    
     class Meta:
-        model: Product
+        model = Product
         fields = ['name', 'cas_number', 'purch_price',
                   'purch_cod', 'description', 'brand',
-                  'sell_cod', 'sell_price', 'product_type']
+                  'sell_cod', 'sell_price', 'product_type', 'supplier']
     
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,9 @@ from project import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    # PRODUCTS -------------------------------------------------------
     path('show_product', views.show_product, name='show_product'),
+    path('create_product_page', views.create_product_page, name='create_product_page'),
     path('create_product', views.create_product, name='create_product')
+    
 ]
